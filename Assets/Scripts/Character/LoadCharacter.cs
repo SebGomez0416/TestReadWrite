61fe136db3f84f036eb48dce50ce787c8892c5e9
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-05-23 06:12:54 -0300

    Add Load Level

--- Assets/Scripts/Character/LoadCharacter.cs
+++ Assets/Scripts/Character/LoadCharacter.cs
@@ -5,8 +5,10 @@ public class LoadCharacter : MonoBehaviour
 {
     public static event Action<Transform,string> LoadPosition;
     
-    private void Awake()
+    private void Start()
     {
-        LoadPosition?.Invoke(transform,SaveCharacter.path);
+       LoadPosition?.Invoke(transform,SaveCharacter.path);
     }
+    
+    
 }
